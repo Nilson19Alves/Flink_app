12492d4bddffd1cb0dde66834e32c1607f8e53cb
Nilson Alves <nilson19alves@gmail.com>
2020-03-22 18:58:01 -0300

    Commit -> add nome do user na view inicio

--- app/src/main/java/com/nilsonalves/flink_app/Flink_Inicio.java
+++ app/src/main/java/com/nilsonalves/flink_app/Flink_Inicio.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.nilsonalves.flink_app.Util.SessionManager;
 import com.nilsonalves.flink_app.jdbc.Connect_Remot;
+
+import java.util.HashMap;
 
 public class Flink_Inicio extends AppCompatActivity {
 
+    private TextView usuario;
     private Button no_Mercado;
     private Button escolher_mercado;
     private Button minhas_listas;
     private Button sair;
+    SessionManager sessionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,7 @@ public class Flink_Inicio extends AppCompatActivity {
     }
 
     private void coletar_ID() {
+        usuario = findViewById(R.id.user);
         no_Mercado = findViewById(R.id.no_Mercado);
         escolher_mercado = findViewById(R.id.escolher_mercado);
         minhas_listas = findViewById(R.id.minhas_listas);
@@ -34,6 +41,11 @@ public class Flink_Inicio extends AppCompatActivity {
     }
 
     private void ections() {
+        sessionManager = new SessionManager(getApplicationContext());
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        String nome = "Olá, " + user.get(sessionManager.NOME) + "!";
+        usuario.setText(nome);
+
         no_Mercado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
