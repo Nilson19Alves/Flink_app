3efd142843f6ce506032c022223bc7375fb176b4
Nilson Alves <nilson19alves@gmail.com>
2020-04-03 12:21:01 -0300

    Commit -> Salvando lista de item usando jdbc Mysql com AsyncTask

--- app/src/main/java/com/nilsonalves/flink_app/Flink_Lista_Concluida.java
+++ app/src/main/java/com/nilsonalves/flink_app/Flink_Lista_Concluida.java
@@ -1,6 +1,7 @@
 package com.nilsonalves.flink_app;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -11,9 +12,15 @@ import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.nilsonalves.flink_app.jdbc.Connect_info;
 import com.nilsonalves.flink_app.lists.List_Adapter_Concluir;
 import com.nilsonalves.flink_app.lists.Lista_Modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Flink_Lista_Concluida extends AppCompatActivity {
     private RecyclerView lista_itens_confirmados;
@@ -55,13 +62,46 @@ public class Flink_Lista_Concluida extends AppCompatActivity {
         concluirCompras.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Flink_Access.class);
-                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeCustomAnimation(getApplicationContext(), R.anim.anima_activity_fade_in, R.anim.anima_atctivity_fade_out);
-                intent.putExtra("fragment", "Lista");
-                ActivityCompat.startActivity(Flink_Lista_Concluida.this, intent, optionsCompat.toBundle());
-                finish();
+                new save_list().execute();
             }
         });
+    }
+
+    // Salvar lista concluida no banco de dados
+    class save_list extends AsyncTask<Void, Void, Void> {
+
+        String sql = "INSERT INTO `Lista` (`ID_User`, `ID_Mercado`, `Item`) VALUES (?, ?, ?);";
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Connection con = Connect_info.connection();
+            for (int i = 0; i < pre_lista_modelo.size(); i++) {
+
+                try {
+                    PreparedStatement preparedStatement = con.prepareStatement(sql);
+                    preparedStatement.setInt(1, 1);
+                    preparedStatement.setInt(2, 2);
+                    preparedStatement.setString(3, pre_lista_modelo.get(i).getItemCompra());
+
+                    preparedStatement.executeUpdate();
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent intent = new Intent(getApplicationContext(), Flink_Access.class);
+            ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeCustomAnimation(getApplicationContext(), R.anim.anima_activity_fade_in, R.anim.anima_atctivity_fade_out);
+            intent.putExtra("fragment", "Lista");
+            ActivityCompat.startActivity(Flink_Lista_Concluida.this, intent, optionsCompat.toBundle());
+            finish();
+            super.onPostExecute(aVoid);
+        }
     }
 
 }
